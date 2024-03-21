7715c988ba1640449120009fad115412423db723
bhupendrasmt <bhupendrasmt@gmail.com>
2024-03-21 11:52:18 +0530

    optimized test search method

--- app/src/test/java/com/app/growtaskapplication/ui/viewmodel/SearchUserViewModelTest.kt
+++ app/src/test/java/com/app/growtaskapplication/ui/viewmodel/SearchUserViewModelTest.kt
@@ -1,29 +1,26 @@
 package com.app.growtaskapplication.ui.viewmodel
 
-import androidx.lifecycle.ViewModelProvider
+import com.app.growtaskapplication.data.api.ApiService
+import com.app.growtaskapplication.data.local.SearchDatabase
 import com.app.growtaskapplication.data.model.*
 import com.app.growtaskapplication.data.repository.SearchUserRepositoryImpl
 import com.app.growtaskapplication.utills.Resource
 import com.app.growtaskapplication.utills.UserType
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
-import javax.inject.Inject
+import java.security.Provider.Service
 
 @RunWith(JUnit4::class)
 class SearchUserViewModelTest {
@@ -31,32 +28,28 @@ class SearchUserViewModelTest {
     @Mock
     lateinit var searchUserRepositoryImpl: SearchUserRepositoryImpl
 
-    lateinit var searchUserViewModel: SearchUserViewModel
+    private lateinit var searchUserViewModel: SearchUserViewModel
 
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
         Dispatchers.setMain(Dispatchers.Unconfined)
+
         searchUserViewModel = SearchUserViewModel(searchUserRepositoryImpl)
     }
 
     @Test
-    fun testViewModel() = runBlocking {
-
-        val spyResult = spy(searchUserViewModel)
+    fun testSearch() = runBlocking {
 
         val query = "hello"
         val type = UserType.ALBUM
 
-        val item = Item(
-            "1", "album", null, null, "", "", "", 5, "", null, 5, "", true, "", null, null, null,
+        val item = Item("1", "album", null, null, "", "", "", 5, "", null, 5, "", true, "", null, null, null,
             100, explicit = true, is_local = true, preview_url = "", track_number = null)
 
-        val listItem: List<Item> = listOf(item)
-        val albums = Data("", listItem, 1, "", 1, "", 1)
-        val expectedSearchResponse = SearchResponse(albums, null, null, null)
-        val expectedResource = Resource.Success(expectedSearchResponse)
+        val expectedResource = createMockSearchResponse(listOf(item))
 
+        //Given
         val queryMap = HashMap<String, String>()
         queryMap["query"] = query
         queryMap["type"] = type.type
@@ -64,10 +57,20 @@ class SearchUserViewModelTest {
         queryMap["offset"] = "0"
         queryMap["limit"] = "20"
 
-
+        // Mocking searchAlbum method behavior
         `when`(searchUserRepositoryImpl.searchAlbum(queryMap, type)).thenReturn(flowOf(expectedResource))
+
+        //When
         searchUserViewModel.searchFlowQuery.value = query
-        spyResult.search(type)
-        assertEquals(expectedResource, searchUserViewModel.albums.value)
+        searchUserViewModel.search(type)
+
+        //Then
+        val actualResource = searchUserViewModel.albums.first()
+        assertEquals(expectedResource, actualResource)
+    }
+
+    private fun createMockSearchResponse(albums: List<Item>): Resource<SearchResponse> {
+        val searchResponse = SearchResponse(Data("", albums, 1, "", 1, "", 1), null, null, null)
+        return Resource.Success(searchResponse)
     }
 }
